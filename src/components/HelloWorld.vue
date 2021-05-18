bf9be48d682b5b4582087aebbd67ef6456017f2c
ジーコマン <kouji.bass.1120@gmail.com>
2021-05-19 03:59:12 +0900

    sixteen-commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,7 @@
 <template>
     <div id="app">
-        <p>{{message}}</p>
         <input type="text" placeholder="郵便番号を入力" v-model="zipcode"/>
-        <button @click="$router.push({getAddress})">住所自動入力</button>
+        <button @click="getAddress()">住所自動入力</button>
         <p>Address: {{Address['Address']}}</p>
     </div>
 </template>
@@ -19,8 +18,9 @@ export default {
   },
   methods: {
         getAddress() {
-      axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.zipcode}?apikey=SHBb270y2vmluvQm8K81yQEsw3BnBtlwmsp8ClH`).
-     then((response) => console.log(response));
+      axios.get(`https://apis.postcode-jp.com/api/v4/postcodes/${this.zipcode}?apikey=SHBb270y2vmluvQm8K81yQEsw3BnBtlwmsp8ClH`)
+     .then((res) => {this.Address = res.data.fullAddress})
+     .catch(()=> {this.Address = ''})
     }
   }
 };
